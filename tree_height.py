40fb9bc381a1d3075dc861b303ef0243d6e3ba96
k0x2 <125827996+k0x2@users.noreply.github.com>
2023-03-06 00:18:01 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,29 +1,49 @@
-# python3
-
 import sys
 import threading
-import numpy
 
 
 def compute_height(n, parents):
-    tree = {}
-    for node, parent in enumerate(parents):
+    children = [[] for _ in range(n)]
+    for i in range(n):
+        parent = parents[i]
         if parent == -1:
-            continue
-        if parent not in tree:
-            tree[parent] = [node]
+            root = i
         else:
-            tree[parent].append(node)
-    def height(node):
-        if node not in tree:
+            children[parent].append(i)
+ 
+    def compute_depth(node):
+        if not children[node]:
             return 1
-        return max([height(child) for child in tree[node]]) + 1
-    root = parents.index(-1)
-    return height(root)
+        max_depth = 0
+        for child in children[node]:
+            depth = compute_depth(child)
+            max_depth = max(max_depth, depth)
+        return max_depth + 1
+
+    return compute_depth(root)
+
+
 def main():
-    n = int(input())
-    parents = list(map(int, input().split()))
-    print(compute_height(n, parents))
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+    input_type = input()
+
+    if 'I' in input_type:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        height = compute_height(n, parents)
+        print(height)
+    elif 'F' in input_type:
+        filename = input()
+        with open("test/" + filename, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+            height = compute_height(n, parents)
+            print(height)
+    else:
+        print("Invalids")
+        exit()
+
+
+
+sys.setrecursionlimit(10**7) 
+threading.stack_size(2**27)  
 threading.Thread(target=main).start()
